091e65f1432821eaf1b20548413d015fb35fc8fc
Nishadi Sasanthika <119886016+NishadiSS@users.noreply.github.com>
2025-09-03 02:23:10 +0530

    Implement full CRUD operations

--- lms_back_end/src/main/java/com/example/lms/dto/auth/RegisterRequest.java
+++ lms_back_end/src/main/java/com/example/lms/dto/auth/RegisterRequest.java
@@ -9,5 +9,5 @@ public class RegisterRequest {
     @NotBlank @Size(min = 3, max = 50) private String username;
     @NotBlank @Size(max = 50) @Email private String email;
     private Set<String> role;
-    @NotBlank @Size(min = 6, max = 40) private String password;
+    @NotBlank @Size(min = 4, max = 40) private String password;
 }
